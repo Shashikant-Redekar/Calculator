dd6ea2a9437969afc26f1a233a8304f6bf1c22e9
Shashikant-Redekar <166703422+Shashikant-Redekar@users.noreply.github.com>
2025-04-30 01:11:04 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Calculator from "./Components/buttons";
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <Calculator />
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ src/Components/buttons.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react"
+
+const Calculator = function (){
+    let [input1, setInput1] = useState('');
+
+    const handleButtonClick = (number) => {
+        let num = input1;
+        num = num + number;
+        setInput1(num);
+    };
+
+    const handleDotClick = (dot) => {
+        let flag = false;
+        for(var i=0; i < input1.length; i++ ){
+            if(input1[i] === '.')
+                flag = true;
+        }
+        if(!flag){
+            let num = input1;
+            num = num + dot;
+            setInput1(num);
+        }
+    }
+
+    return(
+        <div>
+            <div>
+                <input type="text" value={input1} disabled></input>
+            </div>
+            <div>
+                <button>C</button>
+                <button>()</button>
+                <button>%</button>
+                <button>B</button>
+            </div>
+            <div>
+                <button onClick={() => handleButtonClick(7)}>7</button>
+                <button onClick={() => handleButtonClick(8)}>8</button>
+                <button onClick={() => handleButtonClick(9)}>9</button>
+                <button>x</button>
+            </div>
+            <div>
+                <button onClick={() => handleButtonClick(4)}>4</button>
+                <button onClick={() => handleButtonClick(5)}>5</button>
+                <button onClick={() => handleButtonClick(6)}>6</button>
+                <button>-</button>
+            </div>
+            <div>
+                <button onClick={() => handleButtonClick(1)}>1</button>
+                <button onClick={() => handleButtonClick(2)}>2</button>
+                <button onClick={() => handleButtonClick(3)}>3</button>
+                <button>+</button>
+            </div>
+            <div>
+                <button onClick={() => handleDotClick('.')}>.</button>
+                <button onClick={() => handleButtonClick(0)}>0</button>
+                <button>/</button>
+                <button>=</button>
+            </div>
+        </div>
+    )
+}
+
+export default Calculator
